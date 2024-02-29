d8fa9f3e1448b20311de86813dfffb92b98c4185
Mark Arts <marktarts93@gmail.com>
2024-02-29 09:38:39 +0100

    package changes

--- dist/index.js
+++ dist/index.js
@@ -51,6 +51,7 @@ const minimatch_1 = __importDefault(__nccwpck_require__(2002));
 const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
 const OPENAI_API_KEY = core.getInput("OPENAI_API_KEY");
 const OPENAI_API_MODEL = core.getInput("OPENAI_API_MODEL");
+const EXTRA_INSTRUCTIONS = core.getInput("EXTRA_INSTRUCTIONS");
 const octokit = new rest_1.Octokit({ auth: GITHUB_TOKEN });
 const openai = new openai_1.default({
     apiKey: OPENAI_API_KEY,
@@ -113,6 +114,7 @@ function createPrompt(file, chunk, prDetails) {
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
+${EXTRA_INSTRUCTIONS}
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
   
